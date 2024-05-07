5d8092f40ce139862f04f598eb3fb6cd516e407d
Sam Newby <sam.newby19@gmail.com>
2024-05-07 08:52:42 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { createServer } from "node:http";
+import { join } from "node:path"
+import { createApp, createRouter, defineEventHandler, toNodeListener } from "h3";
+import { Eta } from "eta"
+
+
+export const app = createApp();
+
+const router = createRouter();
+
+const eta = new Eta({ views: join(__dirname, "templates") })
+
+app.use(router);
+
+// Add a new route that matches GET requests to / path
+router.get(
+    "/",
+    defineEventHandler((event) => {
+        const html = eta.render('home', {})
+
+        return html;
+    }),
+);
+
+createServer(toNodeListener(app)).listen(4000);
